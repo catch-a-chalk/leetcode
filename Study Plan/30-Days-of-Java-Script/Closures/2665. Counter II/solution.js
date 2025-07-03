716da78224573a007037e34e6feafc3eb05614e0
Denis Makushin <the.makushin@yandex.ru>
2025-07-03 07:14:37 +0300

    Добавлена задача: No.2665

---
+++ Study Plan/30-Days-of-Java-Script/Closures/2665. Counter II/solution.js
@@ -0,0 +1,26 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    let currentValue = init;
+
+    return {
+        increment: function() {
+            return ++currentValue;
+        },
+        decrement: function() {
+            return --currentValue;
+        },
+        reset: function() {
+            return (currentValue = init);
+        }
+    };
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
